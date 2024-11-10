00fca51d8a0a418d362201902cd403116c531eba
Turukano <kennel@posteo.de>
2024-11-10 20:53:42 +0100

    update

--- keymaps/neo/keymap.c
+++ keymaps/neo/keymap.c
@@ -25,14 +25,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [DEF] = LAYOUT(
-    KC_TAB,     KC_J,    KC_D,    KC_U,    KC_A,    KC_X,         KC_P,    KC_H,    KC_L,    KC_M,    KC_W,   DE_SS,    KC_BSPC,
+    KC_TAB,      KC_J,    KC_D,    KC_U,    KC_A,    KC_X,         KC_P,    KC_H,    KC_L,    KC_M,    KC_W,   DE_SS,    KC_BSPC,
     MO(SYM),     KC_C,    KC_T,    KC_I,    KC_E,    KC_O,         KC_B,    KC_N,    KC_R,    KC_S,    KC_G,   KC_Q,        MO(SYM),
-    MO(SHIF),    KC_F,    KC_V,    DE_UDIA, DE_ADIA, DE_ODIA,      DE_Y,    DE_Z,    KC_COMM, KC_DOT,  KC_K,   MO(SHIF),        KC_ENTER,
+    KC_LSFT,    KC_F,    KC_V,    DE_UDIA, DE_ADIA, DE_ODIA,      DE_Y,    DE_Z,    KC_COMM, KC_DOT,  KC_K,   KC_LSFT,        KC_ENTER,
     KC_LCTL,    MO(NAVNUM),   KC_LGUI,   KC_LALT,    KC_BTN1,      KC_SPC,    MO(NAVNUM),   KC_NO,  OSL(CUST), MO(NAVNUM),       KC_RCTL
 ),
 
 [SHIF] = LAYOUT(
-KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+    KC_TAB,     S(KC_J),    S(KC_D),    S(KC_U),    S(KC_A),    KC_X,         KC_P,    KC_H,    KC_L,    KC_M,    KC_W,   DE_SS,    KC_BSPC,
+    MO(SYM),     KC_C,    KC_T,    KC_I,    KC_E,    KC_O,         KC_B,    KC_N,    KC_R,    KC_S,    KC_G,   KC_Q,        MO(SYM),
+    MO(SHIF),    KC_F,    KC_V,    DE_UDIA, DE_ADIA, DE_ODIA,      DE_Y,    DE_Z,    KC_COMM, KC_DOT,  KC_K,   MO(SHIF),        KC_ENTER,
+    KC_LCTL,    MO(NAVNUM),   KC_LGUI,   KC_LALT,    KC_BTN1,      KC_SPC,    MO(NAVNUM),   KC_NO,  OSL(CUST), MO(NAVNUM),       KC_RCTL
 ),
 
 [SYM] = LAYOUT(
