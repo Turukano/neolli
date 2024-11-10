a7ba5e6a87281f824e8a851f6d977c03be2856ba
Turukano <kennel@posteo.de>
2024-11-10 20:46:04 +0100

    srn

---
+++ definitions.h
@@ -0,0 +1,204 @@
+/* Copyright 2015-2016 Matthias Schmidtt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+//#include "keymap.h"
+
+// clang-format off
+
+/*
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ ^ │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ ß │ ´ │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │ Q │ W │ E │ R │ T │ Z │ U │ I │ O │ P │ Ü │ + │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │ A │ S │ D │ F │ G │ H │ J │ K │ L │ Ö │ Ä │ # │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ < │ Y │ X │ C │ V │ B │ N │ M │ , │ . │ - │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define DE_CIRC KC_GRV  // ^ (dead)
+#define DE_1    KC_1    // 1
+#define DE_2    KC_2    // 2
+#define DE_3    KC_3    // 3
+#define DE_4    KC_4    // 4
+#define DE_5    KC_5    // 5
+#define DE_6    KC_6    // 6
+#define DE_7    KC_7    // 7
+#define DE_8    KC_8    // 8
+#define DE_9    KC_9    // 9
+#define DE_0    KC_0    // 0
+#define DE_SS   KC_MINS // ß
+#define DE_ACUT KC_EQL  // ´ (dead)
+// Row 2
+#define DE_Q    KC_Q    // Q
+#define DE_W    KC_W    // W
+#define DE_E    KC_E    // E
+#define DE_R    KC_R    // R
+#define DE_T    KC_T    // T
+#define DE_Z    KC_Y    // Z
+#define DE_U    KC_U    // U
+#define DE_I    KC_I    // I
+#define DE_O    KC_O    // O
+#define DE_P    KC_P    // P
+#define DE_UDIA KC_LBRC // Ü
+#define DE_PLUS KC_RBRC // +
+// Row 3
+#define DE_A    KC_A    // A
+#define DE_S    KC_S    // S
+#define DE_D    KC_D    // D
+#define DE_F    KC_F    // F
+#define DE_G    KC_G    // G
+#define DE_H    KC_H    // H
+#define DE_J    KC_J    // J
+#define DE_K    KC_K    // K
+#define DE_L    KC_L    // L
+#define DE_ODIA KC_SCLN // Ö
+#define DE_ADIA KC_QUOT // Ä
+#define DE_HASH KC_NUHS // #
+// Row 4
+#define DE_LABK KC_NUBS // <
+#define DE_Y    KC_Z    // Y
+#define DE_X    KC_X    // X
+#define DE_C    KC_C    // C
+#define DE_V    KC_V    // V
+#define DE_B    KC_B    // B
+#define DE_N    KC_N    // N
+#define DE_M    KC_M    // M
+#define DE_COMM KC_COMM // ,
+#define DE_DOT  KC_DOT  // .
+#define DE_MINS KC_SLSH // -
+
+/* Shifted symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │ ° │ ! │ " │ § │ $ │ % │ & │ / │ ( │ ) │ = │ ? │ ` │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │   │   │   │   │   │   │   │   │   │   │   │ * │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │ ' │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ > │   │   │   │   │   │   │   │ ; │ : │ _ │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define DE_DEG  S(DE_CIRC) // °
+#define DE_EXLM S(DE_1)    // !
+#define DE_DQUO S(DE_2)    // "
+#define DE_SECT S(DE_3)    // §
+#define DE_DLR  S(DE_4)    // $
+#define DE_PERC S(DE_5)    // %
+#define DE_AMPR S(DE_6)    // &
+#define DE_SLSH S(DE_7)    // /
+#define DE_LPRN S(DE_8)    // (
+#define DE_RPRN S(DE_9)    // )
+#define DE_EQL  S(DE_0)    // =
+#define DE_QUES S(DE_SS)   // ?
+#define DE_GRV  S(DE_ACUT) // ` (dead)
+// Row 2
+#define DE_ASTR S(DE_PLUS) // *
+// Row 3
+#define DE_QUOT S(DE_HASH) // '
+// Row 4
+#define DE_RABK S(DE_LABK) // >
+#define DE_SEMI S(DE_COMM) // ;
+#define DE_COLN S(DE_DOT)  // :
+#define DE_UNDS S(DE_MINS) // _
+
+/* AltGr symbols
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
+ * │   │   │ ² │ ³ │   │   │   │ { │ [ │ ] │ } │ \ │   │       │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
+ * │     │ @ │   │ € │   │   │   │   │   │   │   │   │ ~ │     │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐    │
+ * │      │   │   │   │   │   │   │   │   │   │   │   │   │    │
+ * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤
+ * │    │ | │   │   │   │   │   │   │ µ │   │   │   │          │
+ * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤
+ * │    │    │    │                        │    │    │    │    │
+ * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘
+ */
+// Row 1
+#define DE_SUP2 ALGR(DE_2)    // ²
+#define DE_SUP3 ALGR(DE_3)    // ³
+#define DE_LCBR ALGR(DE_7)    // {
+#define DE_LBRC ALGR(DE_8)    // [
+#define DE_RBRC ALGR(DE_9)    // ]
+#define DE_RCBR ALGR(DE_0)    // }
+#define DE_BSLS ALGR(DE_SS)   // (backslash)
+// Row 2
+#define DE_AT   ALGR(DE_Q)    // @
+#define DE_EURO ALGR(DE_E)    // €
+#define DE_TILD ALGR(DE_PLUS) // ~
+// Row 4
+#define DE_PIPE ALGR(DE_LABK) // |
+#define DE_MICR ALGR(DE_M)    // µ
+
+#define FN_PRINT C(KC_P) //
+#define FN_SEALL C(KC_A) //
+#define FN_LCKSCRN LWIN(KC_L) //
+#define FN_EXPL LWIN(KC_E) //
+#define FN_RUN LWIN(KC_R) //
+#define FN_COPY C(KC_C) //
+#define FN_CUT C(KC_X) //
+#define FN_PASTE C(KC_V) //
+#define FN_UNDO C(DE_Z) //
+#define FN_REDO C(DE_Y) //
+#define FN_NEW C(KC_N) //
+#define FN_SAVE C(KC_S) //
+#define FN_SCRNSHT LWIN(S(KC_S)) //
+#define FN_TXTSHT LWIN(S(KC_T)) //requires powertoys
+#define FN_SHUT A(KC_F4) //
+#define FN_SEARCH C(KC_F) //
+#define FN_PROJECT LWIN(KC_P) //
+#define FN_STREAM LWIN(KC_K) //
+#define FN_ROTATE LWIN(S(KC_R))
+#define FN_ZOOMIN C(DE_PLUS) //
+#define FN_ZOOMOUT C(DE_MINS) //
+
+#define FN_DESKTOP LWIN(KC_D) //
+#define FN_REFRESH KC_F5 //
+#define FN_FULSCRN LWIN(KC_UP) //
+#define FN_DOCKLEFT LWIN(KC_LEFT) //
+#define FN_DOCKRIGHT LWIN(KC_RGHT) //
+#define FN_OSLSHIF OSL(SHIF)
+
+/*Firefox*/
+#define FN_FFLEFT C(S(KC_TAB)) //AutoHotKey #IfWinActive, Firefox Left::^+Tab
+#define FN_FFRIGHT C(KC_TAB) //AutHotKey #IfWinActive, Firefox Right::^Tab
+#define FN_FFNTAB C(KC_T) //alternative: Hold L
+#define FN_FFXTAB C(KC_F4) //Same es undo
+#define FN_FFGOBACK KC_BSPC //BSPC Taste (Also Nav in VSCode) Requires changes in Firefox about:config
+#define FN_FFGOFORTH S(KC_BSPC) //SPC Taste (Also Nav in VSCode)
+#define FN_FFRECOV C(S(KC_T)) //Same as redo
+
+/*Word*/
+#define FN_BOLD C(KC_B) //falsch
+#define FN_ITAL C(KC_I) //falsch
+#define FN_UNDER C(KC_U) //falsch
+#define FN_ALIGNL C(KC_L)
+#define FN_ALIGNC C(KC_E)
+#define FN_ALIGNR C(KC_R)
+#define FN_H1 C(A(KC_1))
+#define FN_BULLET C(S(KC_L)) //
+#define FN_FOOTN C(A(KC_F))
+
+#define FN_NO C(KC_A)
---
+++ keymaps/neo/keymap.c
@@ -0,0 +1,50 @@
+enum layer_names {
+    DEF,
+    SHIF,
+    SYM,
+    NAVNUM,
+    CUST
+};
+
+#include QMK_KEYBOARD_H
+#include "definitions.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------       ----------------------------------------------------.
+ * | Tab  |   J  |   D  |   U  |   A  |   X         |   P  |   H  |   L  |   M  |   W  |  ß  |     Bksp |
+ * |------+------+------+------+------+------       +------+------+------+------+-----+------+----------|
+ * | M3   |   C  |   T  |   I  |   E  |   O         |   B  |   N  |   R  |   S  |   G  |  Q  |  M3 |    |
+ * |------+------+------+------+------+------       +------+------+------+------+------+---------  Enter|
+ * | M2   |   F  |   V  |   Ü  |   Ä  |   Ö         |   Y  |   Z  |   ,  |   .  |   K  | M2        |    |
+ * |------+------+------+------+------+------       +------+------+------+------+------+----------------|
+ * | Ctrl   |  M4  |   Win |   Alt |  Click         |  Space  |      |       |   M6  |  M4  | Strg      |
+ * `-----------------------------------------       ------------------------------------------------'
+ */
+
+
+[DEF] = LAYOUT(
+    KC_TAB,     KC_J,    KC_D,    KC_U,    KC_A,    KC_X,         KC_P,    KC_H,    KC_L,    KC_M,    KC_W,   DE_SS,    KC_BSPC,
+    MO(SYM),     KC_C,    KC_T,    KC_I,    KC_E,    KC_O,         KC_B,    KC_N,    KC_R,    KC_S,    KC_G,   KC_Q,        MO(SYM),
+    MO(SHIF),    KC_F,    KC_V,    DE_UDIA, DE_ADIA, DE_ODIA,      DE_Y,    DE_Z,    KC_COMM, KC_DOT,  KC_K,   MO(SHIF),        KC_ENTER,
+    KC_LCTL,    MO(NAVNUM),   KC_LGUI,   KC_LALT,    KC_BTN1,      KC_SPC,    MO(NAVNUM),   KC_NO,  OSL(CUST), MO(NAVNUM),       KC_RCTL
+),
+
+[SHIF] = LAYOUT(
+KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+),
+
+[SYM] = LAYOUT(
+KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+),
+
+[NAVNUM] = LAYOUT(
+KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+),
+
+[CUST] = LAYOUT(
+KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+),
+
+};
